58c7889740b7e308585d736e708b7366f9453fdd
roswil <rosmerywil@gmail.com>
2019-03-15 07:52:16 -0400

    tercer componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ConversationComponent } from './conversation/conversation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    ConversationComponent
   ],
   imports: [
     BrowserModule,
