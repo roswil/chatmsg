a66cc1e21375979315d69d12892f39470d9e6b3c
roswil <rosmerywil@gmail.com>
2019-03-15 07:53:27 -0400

    cuarto componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ConversationComponent } from './conversation/conversation.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    ConversationComponent
+    ConversationComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
