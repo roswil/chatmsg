0e2a77a24f480c9225bdad63654a7eaa5f96ce0a
roswil <rosmerywil@gmail.com>
2019-03-16 08:20:17 -0400

    mi primera directiva for

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import {User} from '../interfaces/user';
 })
 export class HomeComponent implements OnInit {
 
+  friends: User[];
   constructor() { 
     let myUser:User={
       nick :'Rosmery',
@@ -16,6 +17,43 @@ export class HomeComponent implements OnInit {
       friend:true,
       uid:1
     }
+
+    let usuario1: User = {
+      nick: 'Eduardo',
+      ege:12,
+      email: 'ed@aoe.aoe',
+      friend: true,
+      uid: 1
+    };
+    let usuario2: User = {
+      nick: 'Freddy',
+      ege:12,
+      email: 'fred@aoe.aoe',
+      friend: true,
+      uid: 1
+    };
+    let usuario3: User = {
+      nick: 'Yuliana',
+      ege: 18,
+      email: 'yuli@aoe.aoe',
+      friend: true,
+      uid: 1
+    };
+    let usuario4: User = {
+      nick: 'Ricardo',
+      ege: 17,
+      email: 'rick@aoe.aoe',
+      friend: false,
+      uid: 1
+    };
+    let usuario5: User = {
+      nick: 'Marcos',
+      ege: 30,
+      email:'marcos@aoe.aoe',
+      friend: false,
+      uid: 1
+    };
+    this.friends=[usuario1,usuario2,usuario3,usuario5,myUser];
   }
   /*let users:User[]=[
     myUser
